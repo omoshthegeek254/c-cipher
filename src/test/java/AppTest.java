e4433dadc780971983dcbc6322eacd0b234d7a20
omoshthegeek <omoshthegeek@gmail.com>
2019-12-15 23:35:51 +0300

    added the app test layout

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
+
+    @Test
+    public void main_promptInput_TextString(){
+        //tests go here
+    }
 
 }
