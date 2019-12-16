c5d8aed476ec40cb2883f225b65dbfb86781521d
omoshthegeek <omoshthegeek@gmail.com>
2019-12-16 11:38:18 +0300

    added the general test files minus logic

---
+++ src/test/java/modules/EncryptTest.java
@@ -0,0 +1,20 @@
+package modules;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EncryptTest {
+
+    @Test
+    public void cipherText() {
+    }
+
+    @Test
+    public void getRawText() {
+    }
+
+    @Test
+    public void getShiftKey() {
+    }
+}
